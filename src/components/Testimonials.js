ccff5f9e041cfa7cbd74b2b4d0142f540ace48d3
Kaspars Liepins <kasparslie@gmail.com>
2021-02-18 17:14:15 +0100

    incresed sequelize skill level

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -9,7 +9,8 @@ export default class Testimonials extends Component {
             <div className="two columns header-col">
               <h1><span>Client Testimonials</span></h1>
             </div>
-            <div className="ten columns flex-container">
+            <div className="
+             columns flex-container">
               <div className="flexslider">
                 <ul className="slides">
                   {
